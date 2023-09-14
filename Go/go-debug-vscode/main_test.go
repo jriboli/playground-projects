5d18b1188a54551ac47d3c56aedc6d5768e9d61b
jriboli <jmriboli@yahoo.com>
2023-09-14 10:28:22 -0700

    Missed an update

--- Go/go-debug-vscode/main_test.go
+++ Go/go-debug-vscode/main_test.go
@@ -2,6 +2,9 @@ package main
 
 import "testing"
 
+// Since we have setup the .vscode folder and launch config we can 'Debug" in vscode
+// this includes these test files too
+
 func Test_add(t *testing.T) {
 	a, b, c := 1, 2, 3
 
