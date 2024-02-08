fa6630653f7852ba4ac1f422ffcc72b3676f0adc
jriboli <jmriboli@yahoo.com>
2024-02-08 11:36:45 -0800

    Added new Project - for Java snippets

---
+++ Java/java-snippets/src/main/java/org/example/snippets/DownUpCasting.java
@@ -0,0 +1,49 @@
+package org.example.snippets;
+
+import java.sql.SQLOutput;
+
+public class DownUpCasting {
+
+    public void downCasting() {
+        double d = 4.5;
+        //int i = d; // Incompatible types. Found: 'double', required: 'int'
+        // We can not down cast double to int
+        // But we can typecasting
+        int i = (int) d;
+
+        System.out.println(d);
+    }
+
+    public void upCastClass() {
+        // Up casting moving to parent reference
+        A obj = (A) new B();
+        // Works as this too:
+        // A obj = new B();
+        // Automatically upcasting happens
+        obj.show1();
+
+    }
+
+    public void downCastClass() {
+        // Down casting move to child reference
+        A obj = new B();
+        obj.show1();
+
+        B obj1 = (B) obj;
+        obj1.show2();
+    }
+}
+
+public class A {
+
+    public void show1() {
+        System.out.println(x:"in A show");
+    }
+}
+
+public class B extends A{
+
+    public void show2() {
+        System.out.println(x:"in B show");
+    }
+}
