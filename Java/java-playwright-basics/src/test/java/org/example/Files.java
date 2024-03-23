bcfea8843f5d8358110a27dcfee9fac5b35410e1
jriboli <jmriboli@yahoo.com>
2024-03-22 18:36:46 -0700

    Playwright continued...

--- Java/java-playwright-basics/src/test/java/org/example/Files.java
+++ Java/java-playwright-basics/src/test/java/org/example/Files.java
@@ -1,19 +1,31 @@
 package org.example;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.testng.annotations.Test;
+
+import java.nio.file.Paths;
 
 public class Files {
 
     @Test
-    public void uploadFile() {
+    public void uploadSingleFileTest() {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
         Page page = browser.newPage();
-        page.navigate("https://www.wikipedia.org/");
+        page.navigate("https://www.way2automation.com/way2auto_jquery/registration.php#load_box");
+
+        Locator uploadFile = page.locator("//input[@type='file']");
+        uploadFile.setInputFiles(Paths.get("./src/test/resources/testFiles/unnamed.jpg"));
+
+        System.out.println("----" + uploadFile.getAttribute("name"));
+
+        page.waitForTimeout(5000);
+
+    }
+
+    @Test
+    public void uploadMultipleFilesTest() {
+
     }
 }
