298dfe7facc994700736bf52d2b1dac9ff44dc64
jriboli <jmriboli@yahoo.com>
2024-05-10 11:53:10 -0700

    Initial branch commit

---
+++ java/project-microservice-cqrs-v2/product-command-service/src/test/java/com/binaryNomad/productcommandservice/ProductCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaryNomad.productcommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
