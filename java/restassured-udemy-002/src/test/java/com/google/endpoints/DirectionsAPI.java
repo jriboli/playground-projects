b573bdc9df1c063b762188039dca29c31cd0f9c2
jriboli <jmriboli@yahoo.com>
2024-10-02 11:08:43 -0700

    Secure secrets

--- java/restassured-udemy-002/src/test/java/com/google/endpoints/DirectionsAPI.java
+++ java/restassured-udemy-002/src/test/java/com/google/endpoints/DirectionsAPI.java
@@ -1,13 +1,25 @@
 package com.google.endpoints;
 
+import Config.ConfigLoader;
 import RestUtils.RestUtils;
 import io.restassured.response.Response;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Properties;
 
 public class DirectionsAPI {
 
     String baseEndpoint = "https://maps.googleapis.com/maps/api/";
+    Properties props;
+
+    public DirectionsAPI() {
+        try {
+            props = ConfigLoader.loadProperties("src/test/resources/config.properties");
+        } catch (Exception ex) {
+            props = null;
+        }
+    }
 
     public Response getBasicDirections(String origin, String destination) {
         return getDirectionWithOptionals(origin, destination, null, null);
@@ -21,7 +33,7 @@ public class DirectionsAPI {
         parameters.put("destination", destination);
         parameters.put("avoid", avoid);
         parameters.put("mode", mode);
-        parameters.put("key", "");
+        parameters.put("key", props.getProperty("api.key"));
 
         return RestUtils.performGet(fullEndpoint, parameters);
     }
