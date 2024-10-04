752abf769d47fbde5f21e5043b00a6d28045df79
jriboli <jmriboli@yahoo.com>
2024-10-04 15:56:01 -0700

    Trying out the Fluent Interface on the DirectionsAPI - due to multiple optional fields

--- java/restassured-udemy-002/src/test/java/com/google/Directions_ParameterTesting.java
+++ java/restassured-udemy-002/src/test/java/com/google/Directions_ParameterTesting.java
@@ -1,4 +1,59 @@
 package com.google;
 
-public class Directions_ParameterTesting {
+import com.google.endpoints.DirectionsAPI;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class Directions_ParameterTesting extends DirectionsAPI {
+
+    @Test
+    public void getRouteWithAlternates() {
+        Response res = DirectionsAPI.newDirections("Downey, CA", "Burbank, CA")
+                .alternatives(true)
+                .getDirections();
+        int routes = res.jsonPath().getList("routes").size();
+        //assertTrue(routes > 1, "Routes should be greater than 1");
+        // Use MatcherAssert.assertThat() instead of JUnit's assertTrue() for more descriptive error messages.
+        assertThat("Routes should be greater than 1", routes, greaterThan(1));
+    }
+
+    @Test
+    public void getRouteViaBike() {
+        Response res = DirectionsAPI.newDirections("Downey, CA", "Long Beach, CA")
+                .mode("bicycling")
+                .getDirections();
+        String travelMode = res.jsonPath().getString("routes[0].legs[0].steps[0].travel_mode");
+
+        // Extract all travel_mode values as a complex structure
+        List<List<List<String>>> travelModesComplex = res.jsonPath().getList("routes.legs.steps.travel_mode");
+
+        // Flatten the structure into a single list
+        List<String> travelModesFlat = new ArrayList<>();
+        for (List<List<String>> legs : travelModesComplex) {
+            for (List<String> steps : legs) {
+                travelModesFlat.addAll(steps);
+            }
+        }
+
+        // Now `travelModesFlat` contains all travel_mode values in a single list
+        System.out.println("Flat List of Travel Modes: " + travelModesFlat);
+
+        assertThat(travelModesFlat, everyItem(equalTo("BICYCLING")));
+    }
+
+    @Test
+    public void getRouteViaWalking() {
+        Response res = DirectionsAPI.newDirections("Downey, CA", "Long Beach, CA")
+                .mode("walking")
+                .getDirections();
+    }
 }
--- java/restassured-udemy-002/src/test/java/com/google/endpoints/DirectionsAPI.java
+++ java/restassured-udemy-002/src/test/java/com/google/endpoints/DirectionsAPI.java
@@ -10,31 +10,91 @@ import java.util.Properties;
 
 public class DirectionsAPI {
 
-    String baseEndpoint = "https://maps.googleapis.com/maps/api/";
-    Properties props;
+//    String baseEndpoint = "https://maps.googleapis.com/maps/api/";
+//    Properties props;
+//
+//    public DirectionsAPI() {
+//        try {
+//            props = ConfigLoader.loadProperties("src/test/resources/config.properties");
+//        } catch (Exception ex) {
+//            props = null;
+//        }
+//    }
+//
+//    public Response getBasicDirections(String origin, String destination) {
+//        return getDirectionWithOptionals(origin, destination, null, null, null);
+//    }
+//
+//    public Response getDirectionWithOptionals(String origin, String destination, String avoid, String mode, String alternates) {
+//        String fullEndpoint = baseEndpoint + "directions/json?";
+//
+//        HashMap<String, String> parameters = new HashMap<>();
+//        parameters.put("origin", origin);
+//        parameters.put("destination", destination);
+//        parameters.put("avoid", avoid);
+//        parameters.put("mode", mode);
+//        parameters.put("alternatives", alternates);
+//        parameters.put("key", props.getProperty("api.key"));
+//
+//        return RestUtils.performGet(fullEndpoint, parameters);
+//    }
 
-    public DirectionsAPI() {
-        try {
-            props = ConfigLoader.loadProperties("src/test/resources/config.properties");
-        } catch (Exception ex) {
-            props = null;
+    // Inner Builder class for DirectionsAPI
+    public static class DirectionsBuilder{
+        String baseEndpoint = "https://maps.googleapis.com/maps/api/";
+        Properties props;
+
+        // Mandator Fields
+        private String origin;
+        private String destination;
+        // Optional Fields
+        private String avoid;
+        private String mode;
+        private boolean alternatives;
+
+        public DirectionsBuilder(String origin, String destination) {
+            this.origin = origin;
+            this.destination = destination;
+
+            try {
+                props = ConfigLoader.loadProperties("src/test/resources/config.properties");
+            } catch (Exception ex) {
+                props = null;
+            }
+        }
+
+        public DirectionsBuilder avoid(String avoid) {
+            this.avoid = avoid;
+            return this;
+        }
+
+        public DirectionsBuilder mode(String mode) {
+            this.mode = mode;
+            return this;
+        }
+
+        public DirectionsBuilder alternatives(boolean alternatives) {
+            this.alternatives = alternatives;
+            return this;
+        }
+
+        public Response getDirections() {
+            String fullEndpoint = baseEndpoint + "directions/json?";
+
+            HashMap<String, String> parameters = new HashMap<>();
+            parameters.put("origin", origin);
+            parameters.put("destination", destination);
+            parameters.put("avoid", avoid);
+            parameters.put("mode", mode);
+            parameters.put("alternatives", alternatives + "");
+            parameters.put("key", props.getProperty("api.key"));
+
+            return RestUtils.performGet(fullEndpoint, parameters);
         }
     }
 
-    public Response getBasicDirections(String origin, String destination) {
-        return getDirectionWithOptionals(origin, destination, null, null);
-    }
-
-    public Response getDirectionWithOptionals(String origin, String destination, String avoid, String mode) {
-        String fullEndpoint = baseEndpoint + "directions/json?";
-
-        HashMap<String, String> parameters = new HashMap<>();
-        parameters.put("origin", origin);
-        parameters.put("destination", destination);
-        parameters.put("avoid", avoid);
-        parameters.put("mode", mode);
-        parameters.put("key", props.getProperty("api.key"));
-
-        return RestUtils.performGet(fullEndpoint, parameters);
+    // Static method to initiate the build with mandatory fields
+    public static DirectionsBuilder newDirections(String origin, String destination) {
+        return new DirectionsBuilder(origin, destination);
     }
 }
