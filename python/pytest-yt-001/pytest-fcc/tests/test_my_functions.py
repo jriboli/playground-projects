7e1ea6f654eb923cff09a595ec697919a8a5fac7
jriboli <jmriboli@yahoo.com>
2024-10-29 11:51:42 -0700

    YoutTube PyTest - Initial tests

---
+++ python/pytest-yt-001/pytest-fcc/tests/test_my_functions.py
@@ -0,0 +1,19 @@
+
+import pytest
+import src.my_functions as my_functions
+
+
+def test_add():
+    result = my_functions.add(1, 4)
+    assert result == 5
+
+
+def test_divide():
+    result = my_functions.divide(10, 5)
+    assert result == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(10, 0)
+
