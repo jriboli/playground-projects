60e9227361f30d4fa80ecbb9855ab5cf1bc675e6
jriboli <jmriboli@yahoo.com>
2024-10-29 21:51:05 -0700

    YouTube PyTest - Playing with Mock, Test Classes, etc.

--- python/pytest-yt-001/pytest-fcc/tests/test_my_functions.py
+++ python/pytest-yt-001/pytest-fcc/tests/test_my_functions.py
@@ -1,5 +1,6 @@
 
 import pytest
+import time
 import src.my_functions as my_functions
 
 
@@ -17,3 +18,9 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         my_functions.divide(10, 0)
 
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(10, 5)
+    assert result == 2
