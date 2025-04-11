49e1f7109ecf1936de5e594f1969aebbd3d0e1f5
jriboli <jmriboli@yahoo.com>
2025-04-10 22:53:27 -0700

    Update BlogContent.jsx

--- playground/javascript/react-blog-site-js/src/components/BlogContent.jsx
+++ playground/javascript/react-blog-site-js/src/components/BlogContent.jsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { coding_background } from "../assets";
 import { useParams } from "react-router-dom";
+import StrapiService from "../services/strapiService";
 import ReactMarkdown from "react-markdown"
 
-const BlogContent = ({ blogs }) => {
-  const { id } = useParams();
+const BlogContent = () => {
+  const [blog, setBlog] = useState(null); // Store a single blog object
 
-  let blog = {};
-  if (blog) {
-    let arr = blogs.filter((blog) => blog.id == id);
-    blog = arr[0];
-  } else {
-    blog = {};
+  const { id } = useParams(); // Get the ID from URL params
+
+  // Fetch blog content when the component mounts or the `id` changes
+  useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        const data = await StrapiService.get(`blogs/${id}`); // Fetch the blog by ID
+        setBlog(data); // Set the specific blog object in the state
+      } catch (error) {
+        console.error("Error fetching blog:", error);
+      }
+    };
+
+    if (id) {
+      fetchBlog();
+    }
+  }, [id]); // Only run when `id` changes
+
+  if (!blog) {
+    return <p>Loading...</p>; // Display loading state while fetching blog
   }
-
-  console.log(blog);
 
   return (
     <div className="w-full pb-10 bg-[#f9f9f9]">
       <div className="max-w-[1240px] mx-auto">
         <div className="grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-y-8 px-4 sm:pt-20 md:mt-0 ss:pt-20 text-black">
           <div className="col-span-2 gap-x-8 gap-y-8">
-            <img
+            {/* <img
               className="h-56 w-full object-cover"
               src={`http://127.0.0.1:1337${blog.coverImg[0].url}`}
-            />
+            /> */}
             <h1 className="font-bold text-2xl my-1 pt-5">
               {id}--{blog.blogTitle}
             </h1>
